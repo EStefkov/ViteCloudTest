fc69ea0ff00b18c20ce5b8a0091977ea568a9603
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2024-11-01 13:27:06 +0200

    тест

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,30 @@
-import React from "react";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from "./components/Home";
 import Projects from "./components/Projects";
 import Contact from "./components/Contact";
 import About from "./components/About";
-import "./App.css";
-
+import "./App.css"
 
 function App() {
   return (
     <Router>
       <div className="app">
+
+
+          {/* Хедър секция с изображение и текст */}
+          <div className="header">
+          <img src="./images/1338701.png" alt="Profile" height={200}/>
+          <h1>Emiliyan Stefkov</h1>
+        </div>
+
         <nav>
           <Link to="/">Начало</Link>
           <Link to="/projects">Проекти</Link>
           <Link to="/contact">Контакти</Link>
         </nav>
+
+      
+
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/projects" element={<Projects />} />
