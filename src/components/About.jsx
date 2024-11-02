f75b80ce2b88357f00ce761e9406ae17e2706221
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2024-11-02 09:12:13 +0200

    test push

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,7 @@ function About() {
   return (
     <section className="about">
       <h2>За мен</h2>
-      <p>Кратко описание за мен и моите умения.</p>
+      <p>Кратко описаниеsss за мен и моите умения.</p>
     </section>
   );
 }
