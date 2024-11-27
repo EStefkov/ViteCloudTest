c9fc61ded8fb7b54ba537bde74c04449b6aff679
Emiliyan Stefkov <93265496+EStefkov@users.noreply.github.com>
2024-11-27 10:14:44 +0200

    fixed name

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,13 @@ const Header = () => {
   return (
     <header className="bg-gradient-to-br from-indigo-500 to-purple-600 text-white py-16">
       <div className="max-w-5xl mx-auto text-center">
-        <h1 className="text-6xl font-extrabold">Emiliyan Stefkov</h1>
-        <p className="text-2xl mt-4">Software Engineer & Problem Solver</p>
-        <p className="mt-6 text-lg">
+      <h1 className="text-4xl sm:text-5xl md:text-6xl font-extrabold">
+  Emiliyan Stefkov
+</h1>
+<p className="text-lg sm:text-xl md:text-2xl mt-2 sm:mt-3 md:mt-4">
+  Software Engineer & Problem Solver
+</p>
+ <p className="mt-6 text-lg">
           Crafting modern solutions with a passion for coding and creativity.
         </p>
       </div>
